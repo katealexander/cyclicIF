b03aadb6a53a8dae6fb771b1639e453bef07ff68
Katherine Alexander <katalex115@gmail.com>
2025-03-22 16:07:20 -0400

    adding runIlastikNucleiClassifier.sh

---
+++ runIlastikNucleiClassifier.sh
@@ -0,0 +1,13 @@
+dataDir=$(echo "/Volumes/KAA2025/Rep2_20250205")
+projectName=$(echo "/Users/kalexander/Documents/ilastikModels/nuclearClassificationFFPE.v2/nuclearClassificationFFPE.v2.ilp")
+well=$1
+
+#echo /Volumes/KAA2025/Rep1_20250129/A1/registration/o*.tiff
+#echo $dataDir/$well/registration/o*.tiff
+/Applications/ilastik-1.4.1rc2-arm64-OSX.app/Contents/MacOS/ilastik --headless \
+--project="$dataDir/$projectName" \
+--table_filename="$dataDir/"$well"exportedObjectFeatures.csv" \
+--export_source="Object Predictions" \
+--stack_along="c" \
+"$dataDir/"$well"registration/o*.tiff" \
+--prediction_maps "$dataDir/"$well"registration/DNA_2_Probabilities.tiff"
